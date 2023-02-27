432d84470bdaeb44ee517455bfb0892b8bb8268e
Anil Kumar H V <anilkumarhv04@gmail.com>
2023-02-28 02:06:22 +0530

    added health check url

---
+++ src/main/java/com/anil/airreportscheduler/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.anil.airreportscheduler.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Air Report scheduler is up and running!!!";
+    }
+}
